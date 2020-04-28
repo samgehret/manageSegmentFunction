5ead48142e58e95d4370c0cebf8a0829dc5fe48b
Sam Gehret <sam.gehret@gmail.com>
2020-04-28 14:22:05 -0700

    My first action is ready

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios')
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -9,7 +10,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event meow payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
