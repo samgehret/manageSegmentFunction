598fa2d1bdd0cb075ffd4a76a9881212f6c3c5b0
Sam Gehret <sam.gehret@gmail.com>
2020-04-28 14:58:07 -0700

    someting new

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const github = require('@actions/github');
 const axios = require('axios')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event meow payload: ${payload}`);
-
-axios.get('http://dummy.restapiexample.com/api/v1/employees')
-    .then(function(response) {
-        console.log(response.data)
-        console.log('getting here')
-    })
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('token');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //   console.log(`The event meow payload: ${payload}`);
+    const bodyParams = {
+        function: {
+            display_name: "test api 6",
+            code: "{console.log(test)}",
+            buildpack: "boreal"
+        }
+    }
+    const config = {
+        headers: { Authorization: `Bearer DDeaRf5arluteOr1u9deUu8KcqkcBD621Q1tQb-VX4s.J_QDecOc1ZE7FbCHhoSR2MAvOtPTZRb9uuSR60yZkTU` }
+    };
+    axios.post('https://platform.segmentapis.com/v1beta/workspaces/mOcpeJK5n7/functions',
+        bodyParams,
+        config)
+        .then(function (response) {
+            // console.log(response.data)
+            console.log('getting here')
+        })
 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 // test2
